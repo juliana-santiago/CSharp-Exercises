3b65ecaa3a7d97b9ff118416b65a31cc3a30f31c
juliana-santiago <juju_msantiago@hotmail.com>
2021-02-04 18:22:00 -0300

    corrigindo saldo

--- DepositoBancario/DepositoBancario/ContaBancaria.cs
+++ DepositoBancario/DepositoBancario/ContaBancaria.cs
@@ -14,9 +14,9 @@ namespace DepositoBancario
             Titular = titular;
         }
 
-        public ContaBancaria(int numero, string titular, double saldo) : this (numero, titular)
+        public ContaBancaria(int numero, string titular, double depositoInicial) : this (numero, titular)
         {
-            Saldo = saldo; 
+            Deposito(depositoInicial); 
         }
 
         public void Deposito(double valor)
